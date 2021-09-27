1c2b5809eae008b54f06d6672b67909234e1665e
ray-x <rayx.cn@gmail.com>
2021-09-27 12:41:10 +1000

    debug log

---
+++ lua/utils/log.lua
@@ -0,0 +1,35 @@
+local log_path = vim.fn.expand("$HOME") .. "/tmp/neovim_debug.log"
+
+local log = function(...)
+  local arg = {...}
+  local str = "שׁ "
+  local lineinfo = ''
+
+  local info = debug.getinfo(2, "Sl")
+  lineinfo = info.short_src .. ":" .. info.currentline
+  str = str .. lineinfo
+
+  for i, v in ipairs(arg) do
+    if type(v) == "table" then
+      str = str .. " |" .. tostring(i) .. ": " .. vim.inspect(v) .. "\n"
+    else
+      str = str .. " |" .. tostring(i) .. ": " .. tostring(v)
+    end
+  end
+  if #str > 2 then
+    if log_path ~= nil and #log_path > 3 then
+      local f = io.open(log_path, "a+")
+      io.output(f)
+      io.write(str .. "\n")
+      io.close(f)
+    else
+      print(str .. "\n")
+    end
+  end
+end
+
+
+
+
+
+return {lprint = log}
