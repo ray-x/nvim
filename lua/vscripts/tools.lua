49ec6cf9859de7102aa5ab14b80d3e448a5ffd18
ray-x <rayx.cn@gmail.com>
2021-10-27 08:29:31 +1100

    fix issue #6

--- lua/vscripts/tools.lua
+++ lua/vscripts/tools.lua
@@ -1,2 +1,7 @@
-local cmd = " source " .. vim.fn.expand("$HOME") .. "/.config/nvim/scripts/tools.vim"
+-- local cmd = " source " ..  vim.fn.expand("$HOME") .. "/.config/nvim/scripts/tools.vim"
+local vim_path = require('core.global').vim_path
+local path_sep = require('core.global').path_sep
+
+local cmd = " source " .. vim_path .. path_sep .. "scripts" .. path_sep .. "tools.vim"
+
 vim.api.nvim_exec(cmd, true)
