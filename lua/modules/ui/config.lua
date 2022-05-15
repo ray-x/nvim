89284c97dba2f227c9c15879fe054e3c431cebb9
ray-x <rayx.cn@gmail.com>
2022-05-15 11:31:21 +1000

    gardient highlight for wilder

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -362,7 +362,31 @@ end
 function config.wilder()
   local wilder = require("wilder")
   wilder.setup({ modes = { ":", "/", "?" } })
+  local gradient = {
+    "#f4468f",
+    "#fd4a85",
+    "#ff507a",
+    "#ff566f",
+    "#ff5e63",
+    "#ff6658",
+    "#ff704e",
+    "#ff7a45",
+    "#ff843d",
+    "#ff9036",
+    "#f89b31",
+    "#efa72f",
+    "#e6b32e",
+    "#dcbe30",
+    "#d2c934",
+    "#c8d43a",
+    "#bfde43",
+    "#b6e84e",
+    "#aff05b",
+  }
 
+  for i, fg in ipairs(gradient) do
+    gradient[i] = wilder.make_hl("WilderGradient" .. i, "Pmenu", { { a = 1 }, { a = 1 }, { foreground = fg } })
+  end
   wilder.set_option("pipeline", {
     wilder.branch(
       wilder.cmdline_pipeline({
@@ -374,12 +398,17 @@ function config.wilder()
     ),
   })
   local highlighters = {
+    wilder.highlighter_with_gradient({
+      wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
+    }),
+    wilder.pcre2_highlighter(),
     wilder.basic_highlighter(),
   }
   wilder.set_option(
     "renderer",
     wilder.renderer_mux({
       [":"] = wilder.popupmenu_renderer({
+        highlights = { gradient = gradient },
         min_height = "5%",
         max_height = "35%",
         highlighter = highlighters,
