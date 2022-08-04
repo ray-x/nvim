d9911dca4b7a6c9ef0e15cd8b617371405b8cfc0
ray-x <rayx.cn@gmail.com>
2022-08-04 11:18:37 +1000

    go highlights.scm conceal

--- after/queries/go/highlights.scm
+++ after/queries/go/highlights.scm
@@ -1,6 +1,6 @@
 ;; Keywords
 (("return"   @keyword) (#set! conceal ""))
-(("var"      @keyword) (#set! conceal  "כֿ"))
+(("var"      @keyword) (#set! conceal  "ν"))
 ;; (("if"       @keyword) (#set! conceal "? "))
 ;; (("else"     @keyword) (#set! conceal "! "))
 (("func"     @keyword) (#set! conceal ""))
@@ -16,16 +16,18 @@
 (("continue" @keyword) (#set! conceal "↙"))
 (("struct"   @keyword) (#set! conceal ""))
 (("type"     @keyword) (#set! conceal ""))
-(("interface"       @keyword) (#set! conceal ""))
+(("interface"       @keyword) (#set! conceal ""))
 
 ;; Function names
-((call_expression function: (identifier) @function (#eq? @function "append"  )) (#set! conceal " "))  ;;  
+((call_expression function: (identifier) @function (#eq? @function "append"  )) (#set! conceal "匿"))  ;;  
 
 ;; type
 (((type_identifier) @type (#eq? @type "string")) (#set! conceal ""))
 (((type_identifier) @type (#eq? @type "error")) (#set! conceal ""))
-
+(((type_identifier) @type (#eq? @type "int")) (#set! conceal "כֿ"))
+(((type_identifier) @type (#eq? @type "byte")) (#set! conceal ""))
 (((nil) @type (#set! conceal "ﳠ")))
+(((true) @type (#set! conceal "")))
 ;; fmt.*
 (((selector_expression) @error (#eq? @error "fmt.Println"     )) (#set! conceal ""))
 (((selector_expression) @error (#eq? @error "fmt.Printf"     )) (#set! conceal "狼"))
@@ -34,4 +36,10 @@
 (((pointer_type) @type (#eq? @type "*testing.T")) (#set! conceal "τ"))
 ;; identifiers
 (((identifier) @type (#eq? @type "err"     )) (#set! conceal "ε"))
+(((identifier) @type (#eq? @type "errors"     )) (#set! conceal "é"))
+(((identifier) @type (#eq? @type "request"     )) (#set! conceal "黎"))
+(((identifier) @type (#eq? @type "Request"     )) (#set! conceal "黎"))
+(((identifier) @type (#eq? @type "response"     )) (#set! conceal ""))
+(((identifier) @type (#eq? @type "Response"     )) (#set! conceal ""))
+(((field_identifier) @error (#eq? @error "Error"     )) (#set! conceal "E"))
 ;; (((identifier) @field (#eq? @field "fmt"     )) (#set! conceal ""))
