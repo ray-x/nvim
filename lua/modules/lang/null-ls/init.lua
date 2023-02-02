86b5e8da1635e956e24e36ecc2bc024ce68d2114
ray-x <rayx.cn@gmail.com>
2023-02-02 21:16:57 +1100

    stylua

--- lua/modules/lang/null-ls/init.lua
+++ lua/modules/lang/null-ls/init.lua
@@ -1,6 +1,6 @@
 return {
   config = function()
-    local null_ls = require("null-ls")
+    local null_ls = require('null-ls')
 
     local diagnostics = null_ls.builtins.diagnostics
     local actions = null_ls.builtins.code_actions
@@ -13,124 +13,118 @@ return {
       null_ls.builtins.code_actions.refactoring,
       -- hover.dictionary,
       diagnostics.misspell.with({
-        filetypes = { "markdown", "text", "txt" },
-        args = { "$FILENAME" },
+        filetypes = { 'markdown', 'text', 'txt' },
+        args = { '$FILENAME' },
       }),
       diagnostics.write_good.with({
-        filetypes = { "markdown", "tex", "" },
-        extra_filetypes = { "txt", "text" },
-        args = { "--text=$TEXT", "--parse" },
-        command = "write-good",
+        filetypes = { 'markdown', 'tex', '' },
+        extra_filetypes = { 'txt', 'text' },
+        args = { '--text=$TEXT', '--parse' },
+        command = 'write-good',
       }),
       diagnostics.proselint.with({
-        filetypes = { "markdown", "tex" },
-        extra_filetypes = { "txt", "text" },
-        command = "proselint",
+        filetypes = { 'markdown', 'tex' },
+        extra_filetypes = { 'txt', 'text' },
+        command = 'proselint',
       }),
-      actions.proselint.with({ filetypes = { "markdown", "tex" }, command = "proselint", args = { "--json" } }),
+      actions.proselint.with({ filetypes = { 'markdown', 'tex' }, command = 'proselint', args = { '--json' } }),
     }
 
-    local function exist(bin)
-      return vim.fn.exepath(bin) ~= ""
-    end
+    local function exist(bin) return vim.fn.exepath(bin) ~= '' end
 
     table.insert(
       sources,
       null_ls.builtins.formatting.golines.with({
         extra_args = {
-          "--max-len=180",
-          "--base-formatter=gofumpt",
+          '--max-len=180',
+          '--base-formatter=gofumpt',
         },
       })
     )
 
-    -- shell script
-    if exist("shellcheck") then
+    -- stylua: ignore
+    if exist('shellcheck') then
       table.insert(sources, null_ls.builtins.diagnostics.shellcheck)
     end
 
-    -- shell script
-    if exist("shfmt") then
-      table.insert(sources, null_ls.builtins.formatting.shfmt)
-    end
+    if exist('shfmt') then table.insert(sources, null_ls.builtins.formatting.shfmt) end
 
     -- golang
-    if exist("golangci-lint") then
-      table.insert(sources, null_ls.builtins.diagnostics.golangci_lint)
-    end
+    if exist('golangci-lint') then table.insert(sources, null_ls.builtins.diagnostics.golangci_lint) end
 
-    if exist("revive") then
-      table.insert(sources, null_ls.builtins.diagnostics.revive)
-    end
+    if exist('revive') then table.insert(sources, null_ls.builtins.diagnostics.revive) end
 
     -- docker
-    if exist("hadolint") then
-      table.insert(sources, null_ls.builtins.diagnostics.hadolint)
-    end
+    if exist('hadolint') then table.insert(sources, null_ls.builtins.diagnostics.hadolint) end
 
-    if exist("codespell") then
-      table.insert(sources, null_ls.builtins.diagnostics.codespell)
-    end
-    if exist("eslint_d") then
-      table.insert(sources, null_ls.builtins.diagnostics.eslint_d)
-    end
+    if exist('codespell') then table.insert(sources, null_ls.builtins.diagnostics.codespell) end
+    if exist('eslint_d') then table.insert(sources, null_ls.builtins.diagnostics.eslint_d) end
     -- js, ts
-    if exist("prettierd") then
-      table.insert(sources, null_ls.builtins.formatting.prettierd)
-    end
+    if exist('prettierd') then table.insert(sources, null_ls.builtins.formatting.prettierd) end
     -- lua
-    if exist("selene") then
-      table.insert(sources, null_ls.builtins.diagnostics.selene)
-    end
-
-    if exist("stylua") then
+    if exist('selene') then table.insert(sources, null_ls.builtins.diagnostics.selene) end
+    if exist('stylua') then
       table.insert(
         sources,
         null_ls.builtins.formatting.stylua.with({
-          extra_args = { "--indent-type", "Spaces", "--indent-width", "2", "--column_width", "160", "--collapse-simple-statement", "Always", '--quote-style', 'AutoPreferSingle'}
+          extra_args = {
+            '--indent-type',
+            'Spaces',
+            '--indent-width',
+            '2',
+            '--column-width',
+            '100',
+            '--collapse-simple-statement',
+            'FunctionOnly',
+            '--quote-style',
+            'AutoPreferSingle',
+          },
         })
       )
     end
+    -- stylua: ignore end
 
     table.insert(sources, null_ls.builtins.formatting.trim_newlines)
     table.insert(sources, null_ls.builtins.formatting.trim_whitespace)
     table.insert(
       sources,
-      require("null-ls.helpers").make_builtin({
-        method = require("null-ls.methods").internal.DIAGNOSTICS,
-        filetypes = { "java" },
+      require('null-ls.helpers').make_builtin({
+        method = require('null-ls.methods').internal.DIAGNOSTICS,
+        filetypes = { 'java' },
         generator_opts = {
-          command = "java",
-          args = { "$FILENAME" },
+          command = 'java',
+          args = { '$FILENAME' },
           to_stdin = false,
-          format = "raw",
+          format = 'raw',
           from_stderr = true,
-          on_output = require("null-ls.helpers").diagnostics.from_errorformat([[%f:%l: %trror: %m]], "java"),
+          on_output = require('null-ls.helpers').diagnostics.from_errorformat([[%f:%l: %trror: %m]], 'java'),
         },
-        factory = require("null-ls.helpers").generator_factory,
+        factory = require('null-ls.helpers').generator_factory,
       })
     )
 
     local warn_TODO = {
-      name = "no-todo",
+      name = 'no-todo',
       method = null_ls.methods.DIAGNOSTICS_ON_SAVE,
+      -- stylua: ignore start
       filetypes = {
-        "go",
-        "py",
-        "lua",
-        "sh",
-        "java",
-        "js",
-        "ts",
-        "tsx",
-        "jsx",
-        "html",
-        "css",
-        "scss",
-        "json",
-        "yaml",
-        "toml",
+        'go',
+        'py',
+        'lua',
+        'sh',
+        'java',
+        'js',
+        'ts',
+        'tsx',
+        'jsx',
+        'html',
+        'css',
+        'scss',
+        'json',
+        'yaml',
+        'toml',
       },
+
       generator = {
         fn = function(params)
           local diag = {}
@@ -138,7 +132,7 @@ return {
           -- containing info about the current file and editor state
           for i, line in ipairs(params.content) do
             if line then
-              local f = vim.fn.matchstrpos(line, "\\v(todo)|(fixme)|(xxx)|(fix)|(hack)")
+              local f = vim.fn.matchstrpos(line, '\\v(todo)|(fixme)|(xxx)|(fix)|(hack)')
               local col, end_col = f[2], f[3]
               if col and end_col >= 0 then
                 lprint('found', col, end_col)
@@ -148,8 +142,8 @@ return {
                   row = i,
                   col = col,
                   end_col = end_col + 1,
-                  source = "no-todo",
-                  message = "just do it",
+                  source = 'no-todo',
+                  message = 'just do it',
                   severity = vim.diagnostic.severity.INFO,
                 })
               end
@@ -159,34 +153,25 @@ return {
         end,
       },
     }
-
+    -- stylua: ignore end
     table.insert(sources, warn_TODO)
-    table.insert(sources, require("go.null_ls").gotest())
-    table.insert(sources, require("go.null_ls").gotest_action())
+    table.insert(sources, require('go.null_ls').gotest())
+    table.insert(sources, require('go.null_ls').gotest_action())
     local cfg = {
       sources = sources,
       debug = true,
-      log_level = "info",
+      log_level = 'info',
       debounce = 1000,
       default_timeout = 5000,
       fallback_severity = vim.diagnostic.severity.WARN,
-      root_dir = require("lspconfig").util.root_pattern(
-        ".null-ls-root",
-        "Makefile",
-        ".git",
-        "go.mod",
-        "package.json",
-        "tsconfig.json"
-      ),
+      root_dir = require('lspconfig').util.root_pattern('.null-ls-root', 'Makefile', '.git', 'go.mod', 'package.json', 'tsconfig.json'),
       on_init = function(new_client, _)
-        if vim.tbl_contains({ "h", "cpp", "c" }, vim.o.ft) then
-          new_client.offset_encoding = "utf-16" -- , "utf-32" for ccls
+        if vim.tbl_contains({ 'h', 'cpp', 'c' }, vim.o.ft) then
+          new_client.offset_encoding = 'utf-16' -- , "utf-32" for ccls
         end
       end,
       on_attach = function(client)
-        if client.server_capabilities.documentFormatting then
-          vim.cmd("autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
-        end
+        if client.server_capabilities.documentFormatting then vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()') end
       end,
     }
 
