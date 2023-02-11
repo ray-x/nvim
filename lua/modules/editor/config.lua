8fda60aaa80614d971f16f38f3e5314037c7fc1a
ray-x <rayx.cn@gmail.com>
2023-02-11 12:28:50 +1100

    vmmulti

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -483,13 +483,13 @@ function config.vmulti()
   vim.g.VM_default_mappings = 1
 
   vim.cmd([[
-      let g:VM_maps = {}
-      let g:VM_maps['Select All'] = '<C-M-n>'
-      let g:VM_maps["Add Cursor Down"] = '<M-Down>'
-      let g:VM_maps["Add Cursor Up"] = "<M-Up>"
-      let g:VM_maps["Mouse Cursor"] = "<M-LeftMouse>"
-      let g:VM_maps["Mouse Word"] = "<M-RightMouse>"
-      let g:VM_maps["Add Cursor At Pos"] = '<M-i>'
+    let g:VM_maps = {}
+    let g:VM_maps['Select All'] = '<C-M-n>'
+    let g:VM_maps["Add Cursor Down"] = '<M-Down>'
+    let g:VM_maps["Add Cursor Up"] = "<M-Up>"
+    let g:VM_maps["Mouse Cursor"] = "<M-LeftMouse>"
+    let g:VM_maps["Mouse Word"] = "<M-RightMouse>"
+    let g:VM_maps["Add Cursor At Pos"] = '<M-i>'
   ]])
 end
 
